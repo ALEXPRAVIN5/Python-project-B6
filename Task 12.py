afc7a3a2e013670fa2309760064885287b873632
ALEXPRAVIN5 <120768482+ALEXPRAVIN5@users.noreply.github.com>
2022-12-23 17:24:05 +0530

    Add files via upload

---
+++ Task 12.py
@@ -0,0 +1,31 @@
+'''1) what is class,object,self,init method ?
+
+2) create a student class print the student data ?
+
+3) what are class and instance ,local variables and how to access with examples ?
+
+4) what are the difference b/w method and function ?
+
+5) what is instance method with exampe ?
+
+6) what is class method with example ?
+
+7) what are decorator,generator with example ?
+
+8) what are reduce,map,filter functions with examples ?
+
+9) crete a function that accepts string as a parameter and returns dict from fun
+  # count all alphabets,vowels,consonents,numbers and specialchar and store in dict and return the dict
+  # ex : "He#44AaB$"  op ={alpha:5,num:2,vowels:3,conso:2,spec:2}
+
+10) x=[1,4,3,10] print the binary format each element of list op:['0001','0100','0011','1010']
+
+11) write  example of intance,static,class method with rules?
+
+12) how to access one class data into another class without using inheritance ?
+
+13) what is inheritance ? how many types of inheritances are there with example ?
+
+14) what to access super class data from child class using super method ?
+
+15) what is function overloding and operator overoading with example?'''
